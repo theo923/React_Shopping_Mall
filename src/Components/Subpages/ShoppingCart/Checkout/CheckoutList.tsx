8761aa826df36909e042ab7a10de98c5dc652a66
theo923 <theotam912@gmail.com>
2021-07-19 09:15:26 +0800

    fixed responsive design, images in order view2

--- src/Components/Subpages/ShoppingCart/Checkout/CheckoutList.tsx
+++ src/Components/Subpages/ShoppingCart/Checkout/CheckoutList.tsx
@@ -29,27 +29,30 @@ export const CheckoutList: React.FC<ICheckOutListProps> = ({ userCart, reducer, 
         <div>
             { !ordered ?
             <div>
-                <table className="table w-full">
-                    <thead>
-                        <tr>
-                            <th>Quantity</th> 
-                            <th>Item Description</th> 
-                            <th>Price</th>
-                        </tr>
-                    </thead> 
-                {userCart.map((cartItem: any, idx: number) => <CheckoutItem key={idx} itemprice={cartItem.itemprice} attemptquantity={cartItem.attemptquantity} itemdescription={cartItem.itemdescription} />)}
-                    <tbody>
-                        <tr>
-                            <th></th> 
-                            <th>Price to Pay</th> 
-                            <th>{`$${userCart.reduce(reducer, 0)}`}</th>
-                        </tr>
-                    </tbody> 
-                </table>
-
-                <table className="table w-full">
-                    {<CheckoutInfo eachInfo={deliveryInfo} />}
-                </table>
+                <div className='overflow-x-auto'>
+                    <table className="table w-full">
+                        <thead>
+                            <tr>
+                                <th>Quantity</th> 
+                                <th>Item Description</th> 
+                                <th>Price</th>
+                            </tr>
+                        </thead> 
+                    {userCart.map((cartItem: any, idx: number) => <CheckoutItem key={idx} itemprice={cartItem.itemprice} attemptquantity={cartItem.attemptquantity} itemdescription={cartItem.itemdescription} />)}
+                        <tbody>
+                            <tr>
+                                <th></th> 
+                                <th>Price to Pay</th> 
+                                <th>{`$${userCart.reduce(reducer, 0)}`}</th>
+                            </tr>
+                        </tbody> 
+                    </table>
+                </div>
+                <div className='overflow-x-auto'>
+                    <table className="table w-full">
+                        {<CheckoutInfo eachInfo={deliveryInfo} />}
+                    </table>
+                </div>
                 
                 <div className="justify-end space-x-2 card-actions">
                             <button name='drop' id='drop' onClick={handleOrder} className="btn btn-success" >Order</button>
